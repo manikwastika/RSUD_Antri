16b4131e03ff2967e8779bf6bc13c441add4fdd8
manikwastika <manikwastikafors@gmail.com>
2024-11-20 22:01:32 +0800

    Update RSUD_Antri add about us

--- user_page.php
+++ user_page.php
@@ -22,7 +22,7 @@ if(!isset($_SESSION['pengguna']) || $_SESSION['pengguna'] != 'user'){
     </div>
     <div class="navbar">
         <ul>
-            <li><a href="#">Home</a></li>
+            <li><a href="#" class="active">Home</a></li>
             <li><a href="#">Antri</a></li>
             <li><a href="#">Daftar</a></li>
             <li><a href="#">Riwayat</a></li>
@@ -42,6 +42,60 @@ if(!isset($_SESSION['pengguna']) || $_SESSION['pengguna'] != 'user'){
     </div>
     <div class="arrow-down-icon">
         <img src="assets/image/arrow-down-sign-to-navigate.png" alt="">
+    </div>
+</div>
+
+<div class="container-tentang">
+    <div class="title-tentang">
+        <h1>Tentang Kami</h1>
+    </div>
+    <div class="container-konten-tentang">
+        <div class="card-1">
+            <div class="gambar-card1">
+                <img src="assets/image/gigi.jpg" alt="">
+            </div>
+            <div class="judul-tentang">
+                <h3>Dokter Gigi</h3>
+            </div>
+            <div class="pernyataann-tentang">
+                <p>Deskripsi :</p>
+            </div>
+            <div class="deskripsi">
+                <p>Kedokteran gigi adalah ilmu mengenai pencegahan dan perawatan penyakit atau kelainan pada gigi, mulut,
+                dan maksilofasial melalui tindakan tanpa atau dengan pembedahan.</p>
+            </div>
+        </div>
+        <div class="card-1">
+            <div class="gambar-card1">
+                <img src="assets/image/kulit.jpg" alt="">
+            </div>
+            <div class="judul-tentang">
+                <h3>Dokter Kulit</h3>
+            </div>
+            <div class="pernyataann-tentang">
+                <p>Deskripsi :</p>
+            </div>
+            <div class="deskripsi">
+                <p>Dokter spesialis kulit atau dermatolog adalah dokter yang berspesialisasi pada masalah yang memengaruhi kulit, rambut, dan kuku.</p>
+            </div>
+        </div>
+        <div class="card-1">
+            <div class="gambar-card1">
+                <img src="assets/image/mata.jpeg" alt="">
+            </div>
+            <div class="judul-tentang">
+                <h3>Dokter Mata</h3>
+            </div>
+            <div class="pernyataann-tentang">
+                <p>Deskripsi :</p>
+            </div>
+            <div class="deskripsi">
+                <p>dokter mata yang secara khusus menangani kelainan refraksi mata, yaitu kondisi ketika seseorang tidak mampu melihat suatu objek dengan jelas.</p>
+            </div>
+        </div>
+    </div>
+    <div class="tombol-tentang">
+        <button>Selengkapnya</button>
     </div>
 </div>
 </body>
